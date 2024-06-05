e964fce920ddf82f37354be8927ac7cb5abf858f
Rudra Sankha Sinhamahapatra <rssmp120@gmail.com>
2024-06-05 23:12:49 +0530

    new item added in sidebar

---
+++ Moxsh/apps/user-app/app/(dashboard)/p2p/page.tsx
@@ -0,0 +1,15 @@
+import { SendCard } from "../../../components/SendCard";
+
+
+export default async function home(){
+return(
+    <>
+    <div>
+        P2p
+        <div className="w-full  border border-slate-400">
+        <SendCard/>
+        </div>
+    </div>
+    </>
+)
+}
