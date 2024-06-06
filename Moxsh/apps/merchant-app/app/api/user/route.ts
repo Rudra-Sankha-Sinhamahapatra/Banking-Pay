b6808dc00944471ee986d6009f45a1d3667a9e65
Rudra Sankha Sinhamahapatra <101992909+Rudra-Sankha-Sinhamahapatra@users.noreply.github.com>
2024-06-06 13:55:56 +0530

    Revert "Added workflows"

--- Moxsh/apps/merchant-app/app/api/user/route.ts
+++ Moxsh/apps/merchant-app/app/api/user/route.ts
@@ -3,18 +3,6 @@ import  PrismaClient  from "@repo/db/client" ;
 
 
 export const GET = async () => {
-    const exisitingUser= await PrismaClient.user.findUnique({
-        where: {
-            email:"asd"
-        },
-    })
-
-    if(exisitingUser){
-        return NextResponse.json({
-            message: "User with this email already exists",
-            user: exisitingUser,
-          });
-    }
  const user=  await PrismaClient.user.create({
         data: {
             email: "asd",
@@ -25,6 +13,6 @@ export const GET = async () => {
     })
     return NextResponse.json({
         message: "User created",
-        user:user
+        user
     })
 }
